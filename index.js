91f1ab7a2613155aa7ee501ca33a31318d01f98f
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-03-13 19:25:41 +0300

    Completd assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+
+newHeader.id='victory'
+//document.getElementById('victory')
+newHeader.textContent='Je is the champion'
